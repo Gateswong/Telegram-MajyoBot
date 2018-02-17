f301268f867702e84577d827fb2f3d461ff75da9
Gates_ice <gates@gatesice.com>
2018-02-18 01:30:06 +0800

    Roll feature.

---
+++ MajyoBot/Feature/Roll/RandomSelect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MajyoBot.Feature.Roll
+{
+    public class RandomSelect
+    {
+        public RandomSelect(string title, IReadOnlyList<string> choices)
+        {
+            Title = title;
+            Choices = choices;
+        }
+
+        public readonly string Title;
+        public readonly IReadOnlyList<string> Choices;
+
+        public string Next() 
+        {
+            Random random = new Random();
+            return Choices[random.Next(Choices.Count)];
+        }
+    }
+}
---
+++ MajyoBot/Feature/Roll/Roll.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+namespace MajyoBot.Feature.Roll
+{
+    public class Roll
+    {
+        public bool IsNumbersTooLarge { get; private set; } = false;
+
+        public const int MAX_DICE_RESULT_COUNT = 20;
+
+        private List<BigInteger> diceResults;
+        public List<BigInteger> DiceResults
+        {
+            get
+            {
+                if (IsNumbersTooLarge) { return null; }
+                return diceResults;
+            }
+        }
+
+        public Roll(BigInteger number, BigInteger faces, BigInteger add)
+        {
+            Number = number;
+            Faces = faces;
+            Add = add;
+            IsNumbersTooLarge = (Number > MAX_DICE_RESULT_COUNT);
+
+            PerformRoll();
+        }
+
+        public Roll(BigInteger number, BigInteger faces) : this(number, faces, 0)
+        {
+        }
+
+        public Roll(BigInteger faces) : this(1, faces, 0)
+        {
+        }
+
+        public BigInteger Number { get; private set; }
+        public BigInteger Faces { get; private set; }
+        public BigInteger Add { get; private set; }
+
+        public BigInteger RollResult { get; private set; }
+
+        private void PerformRoll() 
+        {
+            RollResult = Add;
+
+            Random random = new Random();
+            diceResults = new List<BigInteger>();
+
+            if (Faces == 1) { RollResult += Number; }
+            else 
+            {
+                byte[] bytes = Faces.ToByteArray();
+                for (int i = 0; i < Number; i++)
+                {
+                    random.NextBytes(bytes);
+                    bytes[bytes.Length - 1] &= (byte)0x7F;
+                    BigInteger roll = new BigInteger(bytes) % Faces + 1;
+                    if (Number <= MAX_DICE_RESULT_COUNT)
+                    {
+                        DiceResults.Add(roll);
+                    }
+                    RollResult += roll;
+                }
+            }
+        }
+    }
+}

5bbc747ad92e0dddbf9e116ca8b9eed95705d564
Gates_ice <gates@gatesice.com>
2018-02-20 16:35:49 +0800

    Added keyword reply.

---
+++ MajyoBot/MessageHandler/Buzz/KeywordReplyHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace MajyoBot.MessageHandler.Buzz
+{
+    public class KeywordReplyHandler : IMessageHandler
+    {
+        public string Description => "根据关键词回复特定内容";
+
+        public bool HandleMessage(TelegramBotClient bot, Message message)
+        {
+            if (message.Text.Contains("傲娇") && message.Text.ToLower().Contains("bot")) 
+            {
+                bot.SendStickerAsync(
+                    message.Chat.Id,
+                    new FileToSend("CAADAQAD_gAD1i-bBtxkxTuiGVO3Ag"),
+                    replyToMessageId: message.MessageId
+                );
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
